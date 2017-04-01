e580066ac8ce795f72f033494093c55ee638b283
uksist2 <uksist2@gmail.com>
2017-04-01 12:32:05 +0530

    first commite

---
+++ UnitTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UnitTest.Startup))]
+namespace UnitTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
